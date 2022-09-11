75105e20e0e8621aa83b545bd86035fc21e4277c
abhay <akyadav7020@gmail.com>
2022-09-11 15:50:58 +0530

    saved

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def main():
         if request.method == 'POST':
             ch_link = request.form['content']
             count = int(request.form['num'])
-            """ video_id = vd.get_id_of_videos(ch_link,count)
-            if (len(video_id)) == 0:
+            video_id = vd.get_id_of_videos(ch_link,count)
+            """ if (len(video_id)) == 0:
                 return "Invalid Link Try again"
             details = []
             ch_name,ch_url = vd.title_of_channel(ch_link)
@@ -37,7 +37,7 @@ def main():
                 details.append(mydict)
                 database.insert_unique_data("video_link",table_name,mydict)
         return render_template('results.html',details=details[0:(len(details))],n =len(details),name=ch_name) """
-        return render_template('results.html',ch_link=ch_link,count=count,n=1)
+        return render_template('results.html',ch_link=ch_link,count=count,video=video_id,n=1)
 
     except Exception as e:
         return "Try Again"
